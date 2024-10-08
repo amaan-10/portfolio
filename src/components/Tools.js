90f6ef5fbf4342905ce177f09a41828fe538e081
Amaan <amaanshaikh.gg@gmail.com>
2024-10-08 17:16:35 +0530

    tools updated

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -6,7 +6,7 @@ import ToolCard from "./ToolCard";
 const Tools = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center gap-[60px] h-min overflow-hidden relative w-full pt-28">
+      <div className="flex flex-col items-center justify-center gap-[60px] h-min overflow-hidden relative w-full">
         <div className="flex items-start justify-center flex-none flex-col flex-nowrap gap-[10px] h-min overflow-hidden p-0 relative w-full bg-[#fff0]">
           <div className="outline-none flex flex-col justify-start flex-shrink-0 transform-none h-auto relative w-full whitespace-pre-wrap break-words">
             <h1
