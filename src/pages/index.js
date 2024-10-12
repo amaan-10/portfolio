a52985e532934ded0f6ee698a29b1a2ad1be9ac5
Amaan <amaanshaikh.gg@gmail.com>
2024-10-12 23:29:09 +0530

    title updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,11 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width-device width, initial-scale=1" />
+        <title>Amaan's Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main className="flex items-center text-white w-full min-h-screen">
         <Layout className="pt-0">
           <div className="flex gap-[50px] w-full">
