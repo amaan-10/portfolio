a635f3c30c750c08cb962b5ea32a2d48301057ae
Amaan <amaanshaikh.gg@gmail.com>
2024-10-14 21:50:18 +0530

    layout updated

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import React from "react";
 const Layout = ({ children, className = "" }) => {
   return (
     <>
-      <div className={`w-full h-full inline-block z-0 p-32 ${className}`}>
+      <div className={`w-full h-full inline-block z-0 p-[118px] ${className}`}>
         {children}
       </div>
     </>
