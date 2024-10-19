284dae26dcb45ce4b9a1cea37b5026192780c62d
Amaan <amaanshaikh.gg@gmail.com>
2024-10-20 01:25:46 +0530

    Tools Component is now responsive

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -12,10 +12,10 @@ const Tools = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-[60px] h-min overflow-hidden relative w-full">
-        <div className="flex items-start justify-center flex-none flex-col flex-nowrap gap-[10px] h-min overflow-hidden p-0 relative w-full bg-[#fff0]">
-          <div className="outline-none flex flex-col justify-start flex-shrink-0 transform-none h-auto relative w-full whitespace-pre-wrap break-words">
+        <div className="flex items-center lg:items-start justify-center flex-none flex-col flex-nowrap gap-[10px] h-min overflow-hidden p-0 relative w-full bg-[#fff0]">
+          <div className="outline-none flex flex-col  items-center lg:items-start justify-start flex-shrink-0 transform-none h-auto relative w-full whitespace-pre-wrap break-words">
             <h1
-              className="font-bold "
+              className="font-bold text-center lg:text-left"
               style={{ fontSize: 94, lineHeight: "90px" }}
             >
               PREMIUM
@@ -27,7 +27,7 @@ const Tools = () => {
               </p>
             </h1>
           </div>
-          <div className="grid grid-cols-2 gap-[10px] auto-rows-min justify-start w-full relative p-0">
+          <div className="grid grid-cols-3 lg:grid-cols-2 gap-[10px] auto-rows-min justify-start w-full relative p-0">
             <ToolCard
               image={Reactjs}
               title={"Reactjs"}
