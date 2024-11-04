486caafb5ae2a7c75b2e75e6e7862b7c6da93ebe
Amaan <amaanshaikh.gg@gmail.com>
2024-11-04 16:10:08 +0530

    links updated

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export const HomeIcon = () => {
@@ -85,106 +86,110 @@ export const ToolsIcon = () => {
   );
 };
 
-export const GithubIcon = () => {
+export const GithubIcon = ({ link = "/" }) => {
   return (
     <div className="flex-none h-auto w-auto relative">
-      {/* add link */}
-      <div className="flex items-center cursor-pointer flex-row flex-nowrap gap-[10px] h-auto justify-center overflow-hidden p-[6px] relative no-underline w-auto will-change-transform">
-        <div className="">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="pt-[10px]"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="var(--token-733bbd01-4431-42a0-b8b0-48fb9de5f3db, rgb(244, 108, 56))"
-            style={{ width: "150%", height: "150%" }}
-          >
-            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
-          </svg>
+      <Link href={link} target="blank_">
+        <div className="flex items-center cursor-pointer flex-row flex-nowrap gap-[10px] h-auto justify-center overflow-hidden p-[6px] relative no-underline w-auto will-change-transform">
+          <div className="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="pt-[10px]"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="var(--token-733bbd01-4431-42a0-b8b0-48fb9de5f3db, rgb(244, 108, 56))"
+              style={{ width: "150%", height: "150%" }}
+            >
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+            </svg>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
 
-export const TwitterIcon = () => {
+export const TwitterIcon = ({ link = "/" }) => {
   return (
     <div className="flex-none h-auto w-auto relative">
-      {/* add link */}
-      <div className="flex items-center cursor-pointer flex-row flex-nowrap gap-[10px] h-auto justify-center overflow-hidden p-[6px] relative no-underline w-auto will-change-transform">
-        <div className="">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="var(--token-733bbd01-4431-42a0-b8b0-48fb9de5f3db, rgb(244, 108, 56))"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            style={{ width: "100%", height: "100%" }}
-          >
-            <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
-          </svg>
+      <Link href={link} target="blank_">
+        <div className="flex items-center cursor-pointer flex-row flex-nowrap gap-[10px] h-auto justify-center overflow-hidden p-[6px] relative no-underline w-auto will-change-transform">
+          <div className="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="var(--token-733bbd01-4431-42a0-b8b0-48fb9de5f3db, rgb(244, 108, 56))"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              style={{ width: "100%", height: "100%" }}
+            >
+              <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
+            </svg>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
 
-export const InstagramIcon = () => {
+export const InstagramIcon = ({ link = "/" }) => {
   return (
     <div className="flex-none h-auto w-auto relative">
-      {/* add link */}
-      <div className="flex items-center cursor-pointer flex-row flex-nowrap gap-[10px] h-auto justify-center overflow-hidden p-[6px] relative no-underline w-auto will-change-transform">
-        <div className="">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="var(--token-733bbd01-4431-42a0-b8b0-48fb9de5f3db, rgb(244, 108, 56))"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            style={{ width: "100%", height: "100%" }}
-          >
-            <rect x={2} y={2} width={20} height={20} rx={5} ry={5} />
-            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-            <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
-          </svg>
+      <Link href={link} target="blank_">
+        <div className="flex items-center cursor-pointer flex-row flex-nowrap gap-[10px] h-auto justify-center overflow-hidden p-[6px] relative no-underline w-auto will-change-transform">
+          <div className="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="var(--token-733bbd01-4431-42a0-b8b0-48fb9de5f3db, rgb(244, 108, 56))"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              style={{ width: "100%", height: "100%" }}
+            >
+              <rect x={2} y={2} width={20} height={20} rx={5} ry={5} />
+              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+              <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
+            </svg>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
 
-export const EmailIcon = () => {
+export const EmailIcon = ({ link = "/" }) => {
   return (
     <div className="flex-none h-auto w-auto relative">
-      {/* add link */}
-      <div className="flex items-center cursor-pointer flex-row flex-nowrap gap-[10px] h-auto justify-center overflow-hidden p-[6px] relative no-underline w-auto will-change-transform">
-        <div className="">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="var(--token-733bbd01-4431-42a0-b8b0-48fb9de5f3db, rgb(244, 108, 56))"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            style={{ width: "100%", height: "100%" }}
-          >
-            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
-            <polyline points="22,6 12,13 2,6" />
-          </svg>
+      <Link href={link} target="blank_">
+        <div className="flex items-center cursor-pointer flex-row flex-nowrap gap-[10px] h-auto justify-center overflow-hidden p-[6px] relative no-underline w-auto will-change-transform">
+          <div className="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="var(--token-733bbd01-4431-42a0-b8b0-48fb9de5f3db, rgb(244, 108, 56))"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              style={{ width: "100%", height: "100%" }}
+            >
+              <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+              <polyline points="22,6 12,13 2,6" />
+            </svg>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
