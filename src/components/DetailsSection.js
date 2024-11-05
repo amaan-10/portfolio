73762622c5cfbb686d3de4d5eeb6defde0d5f9ae
Amaan <amaanshaikh.gg@gmail.com>
2024-11-05 11:14:38 +0530

    education component added

--- src/components/DetailsSection.js
+++ src/components/DetailsSection.js
@@ -4,6 +4,7 @@ import RecentProjects from "./RecentProjects";
 import Tools from "./Tools";
 import ContactMe from "./ContactMe";
 import dynamic from "next/dynamic";
+import Education from "./Education";
 
 // Dynamically import the AnimatedSection component with SSR disabled
 const AnimatedSection = dynamic(() => import("../components/AnimatedSection"), {
@@ -16,9 +17,12 @@ const DetailsSection = () => {
       <Info />
       <div className="pt-28"></div>
       <AnimatedSection>
+        <Education />
+      </AnimatedSection>
+      <div className="pt-28"></div>
+      <AnimatedSection>
         <RecentProjects />
       </AnimatedSection>
-
       <div className="pt-28"></div>
       <AnimatedSection>
         <Tools />
