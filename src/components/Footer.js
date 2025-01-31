0456abb0fb1aad53fa68430e0f296f74ddf6e558
Amaan <amaanshaikh.gg@gmail.com>
2025-01-31 09:39:27 +0530

    update: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className="pb-10 px-12 text-center dark:text-[#998f8f] text-darkGray overflow-hidden">
         <p className="text-xs ">
           <span className="font-semibold text-[#f46c38]">
-            Amaan Shaikh 2024
+            Amaan Shaikh | 2025
           </span>{" "}
           - Build with NextJS (Page Routes &amp; Server API Actions),
           JavaScript, TailwindCSS, Framer Motion, Nodemailer, Vercel Hosting.
