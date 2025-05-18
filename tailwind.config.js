d7bc8b641fea92b17b0076468c701616fbef5fd8
Amaan <amaanshaikh.gg@gmail.com>
2025-05-18 18:30:07 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,25 +11,31 @@ module.exports = {
       colors: {
         black: "#151312",
         white: "#FFFFFF",
-        nav: "#FFFFFF08", //bg-nav
+        nav: "#FFFFFF08", // bg-nav
         gray: "#998F8F",
         green: "#C5FF41",
         orange: "#F46C38",
         darkGray: "#6A6B6E",
-        softGray: "#B6B4BD33", //#b6b4bd33
+        softGray: "#B6B4BD33",
+      },
+      keyframes: {
+        growBorder: {
+          "0%": { height: "0" },
+          "100%": { height: "100%" },
+        },
+      },
+      animation: {
+        growBorder: "growBorder 2s ease-out forwards",
       },
     },
     screens: {
-      // Custom breakpoints
       xs: "480px", // Extra small devices
       sm: "420px", // Small devices
       md: "810px", // Medium devices (Tablets)
       lg: "1080px", // Large devices (Laptops)
       xl: "1280px", // Extra large devices (Desktops)
       "2xl": "1600px", // Super wide screens
-
-      // Adding a custom "portrait mode" breakpoint
-      portrait: { raw: "(orientation: portrait)" },
+      portrait: { raw: "(orientation: portrait)" }, // Portrait mode
     },
   },
   plugins: [],
